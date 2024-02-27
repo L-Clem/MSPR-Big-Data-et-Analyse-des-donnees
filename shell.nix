94e42bf9b87de55d4b4e3bd004f1d566ac230ee5
Clément <48772756+L-Clem@users.noreply.github.com>
2024-02-27 14:15:44 +0100

    Update shell.nix

---
+++ shell.nix
@@ -0,0 +1,12 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    git
+    python3
+    python311Packages.jupyterlab
+  ];
+}
